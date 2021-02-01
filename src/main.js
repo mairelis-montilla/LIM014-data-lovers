66b21f48480781de78af6af0d0d1c962772523f6
mairelis-montilla <mairelismontilla@gmail.com>
2021-02-01 18:59:04 -0500

    add types on cards

--- src/main.js
+++ src/main.js
@@ -1,48 +1,44 @@
- import {
-   example
- } from './data.js';
- // import data from './data/pokemon/pokemon.js';
+import { example } from './data.js';
+// import data from './data/pokemon/pokemon.js';
 
- // import data from './data/rickandmorty/rickandmorty.js';
+// import data from './data/rickandmorty/rickandmorty.js';
 
- // traer data
- fetch('./data/pokemon/pokemon.json')
+// traer data
 
-   .then(response => response.json())
-   .then(pokemons => {
-     getAllPokemon(pokemons.pokemon);
-   });
+fetch('./data/pokemon/pokemon.json')
+
+  .then( response => response.json())
+  .then( pokemons => { getAllPokemon(pokemons.pokemon);
+  });
 
 
- // Primera función
- function getAllPokemon(pokemons) {
-   for (let pokemon of pokemons) {
+// Primera función
+function getAllPokemon(pokemons) {
+  for (let pokemon of pokemons) {
+    let arrayPokemon = [];
 
-     let arrayPokemon = [];
+    if (pokemon.num <= "030") {
+      arrayPokemon.push({
+        ID: pokemon.num,
+        NAME: pokemon.name,
+        HP: pokemon.stats["max-hp"],
+        CP: pokemon.stats["max-cp"],
+        IMG: pokemon.img,
+        TYPE: pokemon.type,
+      })
 
-     if (pokemon.num <= "030") {
-       arrayPokemon.push({
-         NUM: pokemon.num,
-         NAME: pokemon.name,
-         HP: pokemon.stats["max-hp"],
-         CP: pokemon.stats["max-cp"],
-         IMG: pokemon.img,
-         TYPE: pokemon.type,
-       })
 
-       console.log(arrayPokemon);
+    console.log('Pokemones', arrayPokemon);
 
-     }
-
-     const cardContainer = document.getElementById("mainPokemon"); 
+    const cardContainer = document.getElementById("mainPokemon");
 
 
 
-     cardContainer.innerHTML += `
+    cardContainer.innerHTML += `
 
     <section id="pokemon-card-contrainer">
     <article class="card-contrainer">
-    <p class="id-number">${arrayPokemon[0].NUM}</p>
+    <p class="id-number">${arrayPokemon[0].ID}</p>
           <section class="name-card_container">
             <img src="${arrayPokemon[0].IMG}" alt="">
           <h1 id="namePokemon" class="namePokemon">${arrayPokemon[0].NAME}</h1>
@@ -54,18 +50,19 @@
               <h2>CP</h2>
               <p id="valueCP" class="input">${arrayPokemon[0].CP}</p>
               </div>
-              <div class="column" id="types"> 
-              <h3 class="input">${arrayPokemon[0].TYPE[0]}</h3>
-              <h3 class="input">${arrayPokemon[0].TYPE[1]}</h3>
-
+              <div class="column" id="types">
+                ${arrayPokemon[0].TYPE.map((elemento) => {
+      return `<h3 class="input"> ${elemento} </h3>`
+    })
+      }
 
               </div>
               </article>
             </section>
             </section>
+                    `
 
-                    ` 
+                  }
 
-
-   }
- }
+  }
+}
