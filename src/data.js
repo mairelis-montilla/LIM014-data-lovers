1035ab65bbae536ddac3851b534e0a8e24461abd
mairelis-montilla <mairelismontilla@gmail.com>
2021-02-04 17:07:17 -0500

    feat navigation between pages

--- src/data.js
+++ src/data.js
@@ -1,67 +1,68 @@
- // FILTRAR LOS DATOS POR TIPO
- export const filterDataByType = (data, condition) => {
-   if (condition == "all") {
-     return data;
-   } else {
-     return data.filter(dataFilter => dataFilter.type.includes(condition));
-   }
- };
-
- // FILTRAR LOS DATOS POR NOMBRE
- export const filterDataByName = (data, condition) => {
-  if (condition == "all") {
-    return data;
-  } else {return data.filter(dataFilter => dataFilter.name.includes(condition))
-  }
- };
-
- // FILTRAR LOS DATOS POR REGIÓN
- export const filterDataByRegion = (data, condition) => { 
+// FILTRAR LOS DATOS POR TIPO
+export const filterDataByType = (data, condition) => {
   if (condition == "all") {
     return data;
   } else {
-   return data.filter(dataFilter => dataFilter.generation["name"].includes(condition))
+    return data.filter(dataFilter => dataFilter.type.includes(condition));
   }
- };
-
- // MOSTRAR LOS DATOS POR ORDEN A-Z / Z-A
- export const orderDataByName = (data, condition) => {
-   if (condition == "nameAsc") {
-     // 1 = b va antes que a // -1 = a va antes que b
-     return data.sort((a, b) => (a.name > b.name) ? 1 : -1);
-   } 
-   else {
-     // 1 = a va antes que b // -1 = b va antes que a
-     return data.sort((a, b) => (b.name > a.name) ? 1 : -1);
-   } 
- };
-
- // MOSTRAR LOS DATOS POR ORDEN DE NÚMERO
- export const orderDataByNum = (data, condition) => { 
-   if (condition == "numAsc") { 
-    return data.sort((a, b) => a.num - b.num);
-  } 
-  else { 
-    return data.sort((a, b) => b.num - a.num);
-  } 
 };
 
- // MOSTRAR LOS DATOS POR ORDEN DE CP
- export const orderDataByCP = (data, condition) => { 
-   if (condition == "cpAsc") { 
-    return data.sort((a, b) => a.stats["max-cp"] - b.stats["max-cp"]);
-  } 
-  else { 
-    return data.sort((a, b) => b.stats["max-cp"] - a.stats["max-cp"]);
-  } 
- };
+// FILTRAR LOS DATOS POR NOMBRE
+export const filterDataByName = (data, condition) => {
+  if (condition == "all") {
+    return data;
+  } else {
+    return data.filter(dataFilter => dataFilter.name.includes(condition))
+  }
+};
 
- // MOSTRAR LOS DATOS POR ORDEN DE HP
- export const orderDataByHP = (data, condition) => { 
-   if (condition == "hpAsc") { 
+// FILTRAR LOS DATOS POR REGIÓN
+export const filterDataByRegion = (data, condition) => {
+  if (condition == "all") {
+    return data;
+  } else {
+    return data.filter(dataFilter => dataFilter.generation["name"].includes(condition))
+  }
+};
+
+// MOSTRAR LOS DATOS POR ORDEN A-Z / Z-A
+export const orderDataByName = (data, condition) => {
+  if (condition == "nameAsc") {
+    // 1 = b va antes que a // -1 = a va antes que b
+    return data.sort((a, b) => (a.name > b.name) ? 1 : -1);
+  }
+  else {
+    // 1 = a va antes que b // -1 = b va antes que a
+    return data.sort((a, b) => (b.name > a.name) ? 1 : -1);
+  }
+};
+
+// MOSTRAR LOS DATOS POR ORDEN DE NÚMERO
+export const orderDataByNum = (data, condition) => {
+  if (condition == "numAsc") {
+    return data.sort((a, b) => a.num - b.num);
+  }
+  else {
+    return data.sort((a, b) => b.num - a.num);
+  }
+};
+
+// MOSTRAR LOS DATOS POR ORDEN DE CP
+export const orderDataByCP = (data, condition) => {
+  if (condition == "cpAsc") {
+    return data.sort((a, b) => a.stats["max-cp"] - b.stats["max-cp"]);
+  }
+  else {
+    return data.sort((a, b) => b.stats["max-cp"] - a.stats["max-cp"]);
+  }
+};
+
+// MOSTRAR LOS DATOS POR ORDEN DE HP
+export const orderDataByHP = (data, condition) => {
+  if (condition == "hpAsc") {
     return data.sort((a, b) => a.stats["max-hp"] - b.stats["max-hp"]);
-  } 
-  else { 
+  }
+  else {
     return data.sort((a, b) => b.stats["max-hp"] - a.stats["max-hp"]);
-  } 
- };
+  }
+};
--- src/main.js
+++ src/main.js
@@ -1,31 +1,37 @@
-//import { example } from './data.js';
-import { 
-  
+
+import {
   filterDataByType,
   filterDataByName,
   filterDataByRegion,
   orderDataByName,
   orderDataByNum,
   orderDataByCP,
-  orderDataByHP } from './data.js';
-import data from './data/pokemon/pokemon.js'; 
+  orderDataByHP
+} from './data.js';
+import data from './data/pokemon/pokemon.js';
+
 
 
 const pokemonData = data.pokemon;
-const searchInput = document.querySelector(`[id="searchInput"]`); 
-const showRegion = document.querySelector(`[id="region"]`);
-const showTypes = document.querySelector(`[id="types"]`);
+const searchInput = document.querySelector('#searchInput');
+const showRegion = document.querySelector('#region');
+const showTypes = document.querySelector('[id="types"]');
+const orderBy = document.querySelector('[id="order"]');
+const cardContainer = document.getElementById('mainPokemon');
 
-const orderBy = document.querySelector(`[id="order"]`);
 
-const cardContainer = document.getElementById("mainPokemon"); 
+const listIndexPokemon = document.getElementById('listIndexPokemon');
+const listTopPokemon = document.getElementById('listTopPokemon');
+
+
+
 
 
 let typeSelected;
 /*
 const showPokemonBasic = (allPokemon) => {
   let basicPokemon = [];
-  allPokemon.forEach(pokemon => 
+  allPokemon.forEach(pokemon =>
     basicPokemon.push({
       num: pokemon.num,
       name: pokemon.name,
@@ -38,12 +44,21 @@ const showPokemonBasic = (allPokemon) => {
   return basicPokemon;
 }; */
 
+//NAVEGACIÓN ENTRE PESTAÑAS
 
-// MOSTRAR TODOS LOS POKEMONS 
+listIndexPokemon.addEventListener('click', () => {
+  window.location.assign('./index.html');
+});
+listTopPokemon.addEventListener('click', () => {
+  window.location.assign('./top.html.html');
+});
 
-let showAllPokemon = (allPokemon) => { 
-  allPokemon.forEach(pokemon => { 
-  cardContainer.innerHTML += `
+
+// MOSTRAR TODOS LOS POKEMONS
+
+let showAllPokemon = (allPokemon) => {
+  allPokemon.forEach(pokemon => {
+    cardContainer.innerHTML += `
   <section class="card-contrainer ${pokemon.type[0]}" id="pokemon-card-contrainer">
   <article  >
   <p class="id-number">${pokemon.num}</p>
@@ -54,9 +69,9 @@ let showAllPokemon = (allPokemon) => {
           <section class="info-card_container">
           <div class="column">
             <h2>HP</h2>
-            <p id="valueHP" class="input">${pokemon.stats["max-hp"]}</p>
+            <p id="valueHP" class="input">${pokemon.stats['max-hp']}</p>
             <h2>CP</h2>
-            <p id="valueCP" class="input">${pokemon.stats["max-cp"]}</p>
+            <p id="valueCP" class="input">${pokemon.stats['max-cp']}</p>
             </div>
             <div class="column" id="types">
               ${pokemon.type.map(elemento => {
@@ -68,65 +83,64 @@ let showAllPokemon = (allPokemon) => {
           </section>
           </section>
                   `;
-  
-  })  
+
+  })
   return allPokemon;
-}; 
+};
 
 // PARA MOSTRAR TODOS LOS POKEMONS AL INICIO //
-showAllPokemon(pokemonData); 
+showAllPokemon(pokemonData);
 ///////////////////////////////////////////////
- 
-showTypes.addEventListener(`change`, () => { 
-  const value = showTypes.value;  
-  cardContainer.innerHTML = "";
-  typeSelected = document.querySelector(`[id="types"]`).value;
-  showAllPokemon(filterDataByType(filterDataByType(pokemonData, typeSelected), value));  
-});
- 
-showRegion.addEventListener(`change`, () => { 
-  const value = showRegion.value;   
-  typeSelected = document.querySelector(`[id="types"]`).value;
-  cardContainer.innerHTML = "";
-  showAllPokemon(filterDataByRegion(filterDataByType(pokemonData, typeSelected), value));  
-});
- 
 
-orderBy.addEventListener(`change`, () => { 
-  const value = orderBy.value;  
-
-    cardContainer.innerHTML = "";
-    let data = filterDataByType(pokemonData, typeSelected);
- 
-    if (value == "nameAsc" || value == "nameDesc") {
-      showAllPokemon(orderDataByName(data, value));  
-    }
-    else if (value == "numAsc" || value == "numDesc"){
-      showAllPokemon(orderDataByNum(data, value));  
-    }
-    else if (value == "cpAsc" || value == "cpDesc"){
-      showAllPokemon(orderDataByCP(data, value));  
-    }
-    else if (value == "hpAsc" || value == "hpDesc"){
-      showAllPokemon(orderDataByHP(data, value));  
-    }
-    else {
-      showAllPokemon(pokemonData);
-    }
-    console.log("value = " + value + " typeSelected = "+ typeSelected); 
+showTypes.addEventListener('change', () => {
+  const value = showTypes.value;
+  cardContainer.innerHTML = '';
+  typeSelected = document.querySelector('[id="types"]').value;
+  showAllPokemon(filterDataByType(filterDataByType(pokemonData, typeSelected), value));
 });
 
-// Busqueda 
+showRegion.addEventListener('change', () => {
+  const value = showRegion.value;
+  typeSelected = document.querySelector('[id="types"]').value;
+  cardContainer.innerHTML = '';
+  showAllPokemon(filterDataByRegion(filterDataByType(pokemonData, typeSelected), value));
+});
+
+
+orderBy.addEventListener('change', () => {
+  const value = orderBy.value;
+
+  cardContainer.innerHTML = '';
+  let data = filterDataByType(pokemonData, typeSelected);
+
+  if (value === 'nameAsc' || value === 'nameDesc') {
+    showAllPokemon(orderDataByName(data, value));
+  }
+  else if (value === 'numAsc' || value === 'numDesc') {
+    showAllPokemon(orderDataByNum(data, value));
+  }
+  else if (value === 'cpAsc' || value === 'cpDesc') {
+    showAllPokemon(orderDataByCP(data, value));
+  }
+  else if (value === 'hpAsc' || value === 'hpDesc') {
+    showAllPokemon(orderDataByHP(data, value));
+  }
+  else {
+    showAllPokemon(pokemonData);
+  }
+});
+
+// Busqueda
 searchInput.addEventListener('input', () => {
 
-const pokemonSearch = filterDataByName(pokemonData, searchInput.value.toLowerCase());
+  const pokemonSearch = filterDataByName(pokemonData, searchInput.value.toLowerCase());
 
-if (pokemonSearch.length == 0){
-  cardContainer.innerHTML = "Pokemon no encontrado";
-}
-else {
-  cardContainer.innerHTML = '';
-  showAllPokemon(pokemonSearch);
-}
+  if (pokemonSearch.length == 0) {
+    cardContainer.textContent = 'Pokemon no encontrado';
+  }
+  else {
+    cardContainer.innerHTML = '';
+    showAllPokemon(pokemonSearch);
+  }
 
 })
