c3e6ed30f60ecd816336934b180d5c234be7c753
mairelis-montilla <mairelismontilla@gmail.com>
2021-02-08 10:18:28 -0500

    add funciontion top random

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 // FILTRAR LOS DATOS POR TIPO
 export const filterDataByType = (data, condition) => {
-  if (condition == "all") {
+  if (condition == 'all') {
     return data;
   } else {
     return data.filter(dataFilter => dataFilter.type.includes(condition));
@@ -9,7 +9,7 @@ export const filterDataByType = (data, condition) => {
 
 // FILTRAR LOS DATOS POR NOMBRE
 export const filterDataByName = (data, condition) => {
-  if (condition == "all") {
+  if (condition == 'all') {
     return data;
   } else {
     return data.filter(dataFilter => dataFilter.name.includes(condition))
@@ -18,16 +18,20 @@ export const filterDataByName = (data, condition) => {
 
 // FILTRAR LOS DATOS POR REGIÓN
 export const filterDataByRegion = (data, condition) => {
-  if (condition == "all") {
+  if (condition == 'all') {
     return data;
   } else {
-    return data.filter(dataFilter => dataFilter.generation["name"].includes(condition))
+    return data.filter(dataFilter => dataFilter.generation['name'].includes(condition))
   }
 };
+//FILTRAR DATOS POR NUMERO
+export const filterDataByNum =(data, condition) =>{
+  return data.filter(dataFilter => condition.includes(parseInt(dataFilter.num)))
+}
 
 // MOSTRAR LOS DATOS POR ORDEN A-Z / Z-A
 export const orderDataByName = (data, condition) => {
-  if (condition == "nameAsc") {
+  if (condition == 'nameAsc') {
     // 1 = b va antes que a // -1 = a va antes que b
     return data.sort((a, b) => (a.name > b.name) ? 1 : -1);
   }
@@ -39,7 +43,7 @@ export const orderDataByName = (data, condition) => {
 
 // MOSTRAR LOS DATOS POR ORDEN DE NÚMERO
 export const orderDataByNum = (data, condition) => {
-  if (condition == "numAsc") {
+  if (condition == 'numAsc') {
     return data.sort((a, b) => a.num - b.num);
   }
   else {
@@ -49,20 +53,20 @@ export const orderDataByNum = (data, condition) => {
 
 // MOSTRAR LOS DATOS POR ORDEN DE CP
 export const orderDataByCP = (data, condition) => {
-  if (condition == "cpAsc") {
-    return data.sort((a, b) => a.stats["max-cp"] - b.stats["max-cp"]);
+  if (condition == 'cpAsc') {
+    return data.sort((a, b) => a.stats['max-cp'] - b.stats['max-cp']);
   }
   else {
-    return data.sort((a, b) => b.stats["max-cp"] - a.stats["max-cp"]);
+    return data.sort((a, b) => b.stats['max-cp'] - a.stats['max-cp']);
   }
 };
 
 // MOSTRAR LOS DATOS POR ORDEN DE HP
 export const orderDataByHP = (data, condition) => {
-  if (condition == "hpAsc") {
-    return data.sort((a, b) => a.stats["max-hp"] - b.stats["max-hp"]);
+  if (condition == 'hpAsc') {
+    return data.sort((a, b) => a.stats['max-hp'] - b.stats['max-hp']);
   }
   else {
-    return data.sort((a, b) => b.stats["max-hp"] - a.stats["max-hp"]);
+    return data.sort((a, b) => b.stats['max-hp'] - a.stats['max-hp']);
   }
 };
--- src/top.js
+++ src/top.js
@@ -1,12 +1,56 @@
 import data from './data/pokemon/pokemon.js';
-
+import{filterDataByNum} from './data.js'
 const listPokemonTop = document.getElementById('listPokemonTop');
 const listInicioTop = document.getElementById('listInicioTop');
+const mainContainer= document.getElementById('mainPokemon')
 const pokemons = data.pokemon;
-console.log(pokemons);
 
 listPokemonTop.addEventListener('click',()=>{
   window.location.assign('./pokemon.html');
 });
 listInicioTop.addEventListener('click',()=>{ window.location.assign('./index.html');
 });
+
+let showAllPokemon = (allPokemon) => {
+  allPokemon.forEach(pokemon => {
+    mainContainer.innerHTML += `
+  <section class="card-contrainer ${pokemon.type[0]}" id="pokemon-card-contrainer">
+  <article  >
+  <p class="id-number">${pokemon.num}</p>
+        <section class="name-card_container">
+          <img class="image-pokemon" src="${pokemon.img}" alt="${pokemon.name}>
+        <h1 id="namePokemon" class="namePokemon">${pokemon.name}</h1>
+        </section>
+          <section class="info-card_container">
+          <div class="column">
+            <h2>HP:</h2>
+            <p id="valueHP" class="input">${pokemon.stats['max-hp']}</p>
+            <h2>CP:</h2>
+            <p id="valueCP" class="input">${pokemon.stats['max-cp']}</p>
+            </div>
+            <div class="column" id="types">
+              ${pokemon.type.map(elemento => {
+      return `<h3 class="input ${elemento}"> ${elemento}</h3>`
+    })
+      }
+            </div>
+            </article>
+          </section>
+          </section>
+                  `;
+
+  })
+  return allPokemon;
+};
+
+  let numRandom=[];
+  for(let i=0; i<10;i++){
+
+      numRandom.push( Math.floor(Math.random()*(251)));
+
+  }
+  console.log('¿que devuelve?', filterDataByNum(pokemons, numRandom))
+
+let topPokemon = filterDataByNum(pokemons, numRandom);
+
+showAllPokemon(topPokemon)
