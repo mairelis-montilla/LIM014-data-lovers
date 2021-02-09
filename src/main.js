76c0b5a7f4c7d23cfec0d3fabfe7730a6cd90eff
Milumon <17993682+Milumon@users.noreply.github.com>
2021-02-09 10:31:44 -0500

    fix loading

--- src/main.js
+++ src/main.js
@@ -47,31 +47,40 @@ listTopPokemon.addEventListener('click', () => {
 
 // MOSTRAR TODOS LOS POKEMONS
 
-const showAllPokemon = (allPokemon) => {
-  allPokemon.forEach((pokemon) => {
-    cardContainer.innerHTML += `
-  <section class="card-contrainer ${pokemon.type[0]}" id="pokemon-card-contrainer">
-  <article  >
-  <p class="id-number">${pokemon.num}</p>
-        <section class="name-card_container">
-          <img class="image-pokemon" src="${pokemon.img}" alt="${pokemon.name}>
-        <h1 id="namePokemon" class="namePokemon">${pokemon.name}</h1>
-        </section>
-          <section class="info-card_container">
-          <div class="column">
-            <h2>HP:</h2>
-            <p id="valueHP" class="input">${pokemon.stats['max-hp']}</p>
-            <h2>CP:</h2>
-            <p id="valueCP" class="input">${pokemon.stats['max-cp']}</p>
-            </div>
-            <div class="column" id="types">
-              ${pokemon.type.map((elemento) => `<h3 class="input ${elemento}"> ${elemento}</h3>`)}
-            </div>
-            </article>
+let showAllPokemon = (allPokemon) => {
+  allPokemon.forEach(pokemon => {
+    console.log("tiempo", pokemon)
+    var container = document.createElement('section');
+    container.className = "card-contrainer " + pokemon.type[0];
+ 
+    
+    cardContainer.appendChild(container).innerHTML = `
+     
+    <article>
+    <p class="id-number">${pokemon.num}</p>
+          <section class="name-card_container">
+            <img class="image-pokemon" src="${pokemon.img}" alt="${pokemon.name}>
+          <h1 id="namePokemon" class="namePokemon">${pokemon.name}</h1>
           </section>
-          </section>`;
-  });
-  return allPokemon;
+            <section class="info-card_container">
+            <div class="column">
+              <h2>HP:</h2>
+              <p id="valueHP" class="input">${pokemon.stats['max-hp']}</p>
+              <h2>CP:</h2>
+              <p id="valueCP" class="input">${pokemon.stats['max-cp']}</p>
+              </div>
+              <div class="column" id="types">
+                ${pokemon.type.map(elemento => {
+        return `<h3 class="input ${elemento}"> ${elemento}</h3>`
+      })
+        }
+              </div>
+              </article>
+            </section> 
+                    `;
+
+
+  })
 };
 
 // PARA MOSTRAR TODOS LOS POKEMONS AL INICIO //
