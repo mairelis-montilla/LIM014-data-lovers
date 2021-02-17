6b0e687a960f1df4420b92abac2a1ba66e5cce04
mairelis-montilla <mairelismontilla@gmail.com>
2021-02-17 14:22:47 -0500

    add logo, option  disable to select

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const listPokemonIndex = document.getElementById('listIndexPokemon');
 const listTopPokemon = document.getElementById('listTopPokemon');
-//const homeButton = document.getElementById('homeButton');
+const homeButton = document.getElementById('homeButton');
 //NAVEGACIÓN ENTRE PESTAÑAS
 
-//homeButton.addEventListener('click', () => {
-  //window.location.assign('./pokemon.html');});
+homeButton.addEventListener('click', () => {
+  window.location.assign('./pokemon.html');});
 
 listTopPokemon.addEventListener('click', () => {
   window.location.assign('./top.html');
