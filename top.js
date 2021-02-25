7bab5771c5f539eac6ad4ea36cd5eb08f516585a
Milumon <17993682+Milumon@users.noreply.github.com>
2021-02-24 20:04:54 -0500

    Updates

--- top.js
+++ top.js
@@ -230,8 +230,8 @@ iconArrow.addEventListener('click', () => {
 
 function toggleImg() {
   let imgSRC = iconArrow.src;
-  imgSRC.includes('/images/arrowBottom.svg') ?
-    imgSRC = '/images/arrowTop.svg' : imgSRC = '/images/arrowBottom.svg';
+  imgSRC.includes('./images/arrowBottom.svg') ?
+    imgSRC = './images/arrowTop.svg' : imgSRC = './images/arrowBottom.svg';
   return imgSRC;
 }
 
